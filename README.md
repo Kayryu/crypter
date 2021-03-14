c940b15d0c1b7af4209c8b5ee406aeb6cf575b72
kayryu <lukayryu@gmail.com>
2021-03-14 17:45:57 +0800

    add rust example, rust-ffi.  init dart example

---
+++ README.md
@@ -0,0 +1,21 @@
+# crypter
+
+A cross platform cryptograph library.
+
+## Requires
+
+- Rust
+- Dart
+- Flutter
+
+## Docs
+
+- [flutter-rust-ffi](https://github.com/brickpop/flutter-rust-ffi/blob/master/README.md)
+
+## Support crypto
+
+## Support Platform
+
+
+
+# Flow
---
+++ signer-ffi/src/lib.rs
@@ -0,0 +1,42 @@
+use std::os::raw::{c_char};
+use std::ffi::{CString, CStr};
+
+#[no_mangle]
+pub extern "C" fn add(a: i64, b: i64) -> i64 {
+    add(a, b)
+}
+
+#[no_mangle]
+pub extern fn rust_greeting(to: *const c_char) -> *mut c_char {
+    let c_str = unsafe { CStr::from_ptr(to) };
+    let recipient = match c_str.to_str() {
+        Err(_) => "there",
+        Ok(string) => string,
+    };
+
+    CString::new("Hello ".to_owned() + recipient).unwrap().into_raw()
+}
+
+#[no_mangle]
+pub extern fn rust_cstr_free(s: *mut c_char) {
+    unsafe {
+        if s.is_null() { return }
+        CString::from_raw(s)
+    };
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rust_add() {
+        assert_eq!(3, signer::add(1, 2));
+    }
+
+    #[test]
+    fn test_ffi_add() {
+        assert_eq!(3, add(1, 2));
+    }
+}
