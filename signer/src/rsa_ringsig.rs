58722cb83ff270231acb66bf51449df6dccd99d6
kayryu <lukayryu@gmail.com>
2021-11-26 22:17:50 +0800

    Add test

--- signer/src/rsa_ringsig.rs
+++ signer/src/rsa_ringsig.rs
@@ -15,18 +15,23 @@ pub struct Ring {
     // the private key
     pub piv_key: RsaPrivateKey,
     // the index of private key in group
-    pub l: u32,
+    pub i: u32,
     // rsa bit size
     pub S: usize,
+    // the maximum of rand value
+    pub Q: BigUint,
 }
 
 impl Ring {
-    pub fn new(pub_keys: Vec<RsaPublicKey>, piv_key: RsaPrivateKey, l: u32) -> Self {
+    pub fn new(pub_keys: Vec<RsaPublicKey>, piv_key: RsaPrivateKey, i: u32) -> Self {
+        let S = 64;
+        let Q = BigUint::one() << S;
         Self {
             pub_keys,
             piv_key,
-            l,
-            S: 64,
+            i,
+            S,
+            Q
         }
     }
 }
@@ -65,9 +70,11 @@ pub fn gen_keypair<R: CryptoRng + RngCore>(mut rng: R, bit_size: usize) -> (RsaP
     ys = v / Ck,v(y1, y2,..., yn)
     xs = gs-1(ys)
     signature = (p1, p2, ...., pn, x1, x2, ...xs, .., xn, v)
+
+    Ck,v(y1,y2,...,yn) = y1 xor y2 xor .... yn
 */
 pub fn sign(ring: &Ring, msg: &[u8]) -> Signature {
-    assert!(ring.pub_keys.len() >= ring.l as usize);
+    assert!(ring.pub_keys.len() >= ring.i as usize);
 
     let mut rng = rand::thread_rng();
 
@@ -77,7 +84,7 @@ pub fn sign(ring: &Ring, msg: &[u8]) -> Signature {
     let mut xi:Vec<BigUint> = (0..n).map(|_| rng.gen_biguint(ring.S.clone())).collect();
     let mut yi = vec![];
     for i in 0..xi.len() {
-        let g = gx(&xi[i],&ring.pub_keys[i]);
+        let g = gx(&xi[i],&ring.pub_keys[i], BigUint::one() << 16);
         yi.push(g);
     }
     let mut r = v.clone();
@@ -88,7 +95,11 @@ pub fn sign(ring: &Ring, msg: &[u8]) -> Signature {
 
     // latest
     let d = ek(&v, &k);
-    let yt = d - r;
+    let yt = if d < r {
+        d + ring.piv_key.n() -r
+    } else {
+        d - r
+    };
     let xt = dgx(&yt, &ring.piv_key);
     xi.push(xt);
     Signature::new(ring.pub_keys.clone(), xi, v)
@@ -106,7 +117,7 @@ pub fn verify(sig: &Signature, msg: &[u8]) -> bool {
 
     let mut r = sig.v.clone();
     for i in 0..sig.xi.len() {
-        r = r + gx(&sig.xi[i],&sig.pki[i]);
+        r = r + gx(&sig.xi[i],&sig.pki[i], BigUint::one() << 16);
         r = ek(&r, &k);
     }
 
@@ -119,11 +130,30 @@ fn hash(msg: &[u8]) -> BigUint {
     BigUint::from_bytes_be(&hasher.finalize().to_vec())
 }
 
-fn gx(x: &BigUint, pub_key: &RsaPublicKey) -> BigUint {
+/// Computes the extended trap-door permutation `g(x)`.
+fn gx(x: &BigUint, pub_key: &RsaPublicKey, common_m: BigUint) -> BigUint {
+    let Q = BigUint::one() << 16;
+    let (mut q, mut r) = x.div_mod_floor(pub_key.n());
+    let t = (q.clone() + 1u32) * pub_key.n();
+    let mut ret = x.clone();
+    if t < Q {
+        ret = q * pub_key.n() + gx2(&r, pub_key);
+    }
+    return ret;
+}
+
+fn gx2(x: &BigUint, pub_key: &RsaPublicKey) -> BigUint {
     x.modpow(pub_key.e(), pub_key.n())
 }
 
+/// Computes the inverse function of `g(x)`
+///
 fn dgx(x: &BigUint, priv_key: &RsaPrivateKey) -> BigUint {
+    // y = g(x) = q * n + f(r)
+    let q = x / priv_key.n();
+    // y - q * n = f(r)
+    let fr = x - q * priv_key.n();
+    // f^{-1}(y - q * n) = r
     x.modpow(priv_key.d(), priv_key.n())
 }
 
@@ -140,7 +170,7 @@ fn test_gx() {
     println!("priv_key: {:?}", priv_key);
     let msg: &[u8] = b"123456";
     let x = BigUint::from_bytes_be(msg);
-    let enc = gx(&x, &pub_key);
+    let enc = gx(&x, &pub_key, BigUint::one() << 16);
     let dec = dgx(&enc, &priv_key);
 
     println!("enc: {}, dec: {}",enc, dec);
@@ -158,35 +188,59 @@ fn test_ek() {
         gen_keypair(&mut rng, RSA_SIZE)
     }).collect();
 
-    pairs.iter().for_each(|p| println!("sk:{:?}, pk:{:?}", p.0, p.1));
-    xi.iter().for_each(|x|println!("x: {}", x));
+    pairs.iter().for_each(|p| println!("sk: {:?}, pk: {:?}",p.0, p.1));
 
+    pairs.iter().for_each(|p| println!("sk-d:{:018b} sk-e:{:018b}, pk-n:{:018b} pk-e:{:018b}",
+                                       p.0.d(),p.0.e(), p.1.n(), p.1.e()));
+    xi.iter().for_each(|x|println!("x: {:018b}", x));
+
+    // k = hash(msg)
     let k = rng.gen_biguint(RSA_SIZE);
     let v = rng.gen_biguint(RSA_SIZE);
 
-    println!("v: {} \nk: {}", v, k);
+    println!("v: {:018b} \nk: {:018b}", v, k);
     let mut lr = v.clone();
     for i in (0..n-1) {
-        let y = gx(&xi[i],&pairs[i].1);
-        lr = lr + y;
+        lr = lr.mod_floor(&pairs[i].1.n());
+        lr = lr + gx(&xi[i],&pairs[i].1, BigUint::one() << 16);
+        lr = lr.mod_floor(&pairs[i].1.n());
         lr = ek(&lr, &k);
+        println!("l-{} : {:018b}",i, lr);
     }
 
-    println!("{}", lr);
+    println!("lr {:018b}", lr);
+    lr.mod_floor(pairs[n-1].1.n());
+    println!("lr mod {:018b}", lr);
 
     // Ek(r, k)=v,  r = lr + yt
     let mut d = ek(&v, &k);
-    d = d - lr;
+    println!("d {:018b}", d);
+    d = d.mod_floor(pairs[n-1].1.n());
+    println!("d {:018b}, n {:018b}", d, pairs[n-1].1.n());
+    d = if d < lr {
+        println!("@@@@@@@@@@");
+        pairs[n-1].1.n() + d - lr
+    } else {
+        println!("#########");
+        d - lr
+    };
+    println!("d {:018b}", d);
     let xt = dgx(&d, &pairs[n-1].0);
+    println!("t {:018b}", xt);
+    let rev = gx(&xt, &pairs[n-1].1, BigUint::one() << 16);
+    println!("r {:018b}", rev);
     xi[n-1] = xt;
 
     let mut z = v.clone();
     for i in 0..xi.len() {
-        z = z + gx(&xi[i],&pairs[i].1);
+        z = z.mod_floor(&pairs[i].1.n());
+        z = z + gx(&xi[i],&pairs[i].1, BigUint::one() << 16);
+        z = z.mod_floor(&pairs[i].1.n());
         z = ek(&z, &k);
     }
 
     println!("z: {}, v: {}", z, v);
+    assert!(z == v);
 }
 
 #[test]
